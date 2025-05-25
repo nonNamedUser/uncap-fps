01bc677ea50067e5d2b6a7f45e3f5ec24a40a93f
caspergtag <109670694+nonNamedUser@users.noreply.github.com>
2025-05-25 11:51:42 +0200

    Create uncap_studio.bat

---
+++ src/uncap_studio.bat
@@ -0,0 +1,27 @@
+:: RobloxStudio Version
+
+@echo off
+setlocal
+
+call "./kill/studio.bat"
+
+set "fps="
+for /f "usebackq delims=" %%a in ("fps.txt") do (
+    set "fps=%%a"
+    goto :gg
+)
+
+:gg
+
+if "%fps%"=="" (
+    exit /b 1
+)
+
+powershell -NoProfile -Command ^
+  "$xmlPath = Join-Path $env:LOCALAPPDATA 'Roblox\GlobalBasicSettings_13_Studio.xml';" ^
+  "[xml]$xml = Get-Content $xmlPath;" ^
+  "$node = $xml.roblox.Item.Properties.int | Where-Object { $_.name -eq 'FramerateCap' };" ^
+  "if ($node) { $node.'#text' = '%fps%'; $xml.Save($xmlPath); Write-Host 'FPS set to %fps%' } else { Write-Host 'FramerateCap not found!' }"
+
+
+pause
